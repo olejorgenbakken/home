7a0f152bfe412cef895f6b9c937e7b5e42c14cc7
OJ <olejorgenbakken@icloud.com>
2021-08-19 15:13:31 +0200

    Text updates

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -5,9 +5,9 @@ function Home(props) {
   return (
     <section>
       <Hero
-        title="Velkommen"
+        title="Hei 👋"
         backgroundImg="https://images.unsplash.com/photo-1608255726239-8d44c46cdd58?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3024&q=80"
-        message="Denne nettsiden er veldig WIP ⚙️"
+        message="Jeg heter Ole Jørgen, og jeg designer og utvikler ting på internett. Denne nettsiden er forresten langt fra ferdig."
       />
       <section className="width-900">
         <ExternalList />

45e8f59afac975af407f5b158146bc9e165de624
OJ <olejorgenbakken@icloud.com>
2021-09-12 16:31:53 +0200

    remove firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-import loadable from '@loadable/component';
-
-
-import { Component } from "react";
-import { RouteComponentProps, withRouter } from 'react-router';
 import { Route, Switch } from "react-router-dom";
 
 import Header from "./components/header/header";
@@ -10,41 +5,35 @@ import Home from "./Views/Home/Home";
 import CV from "./Views/CV/Cv";
 import NotFound from "./Views/NotFound/NotFound";
 import Footer from "./components/footer/footer";
+import ProjectRouter from "./Views/Projects/ProjectRouter";
 
 import "./styles/global.css";
 
-const ProjectsRouter = loadable(() => import("./Views/Projects/ProjectRouter"));
 
-class App extends Component<RouteComponentProps, any> {
-  componentDidUpdate() {
-    window.scrollTo(0, 0);
-  }
 
-  render() {
-    return (
-      <Switch>
-        <Route path="/prosjekter">
-          <ProjectsRouter />
-        </Route>
-        <Route exact path="/cv">
-          <Header />
-          <CV />
-          <Footer />
-        </Route>
-        <Route exact path="/">
-          <Header />
-          <Home />
-          <Footer />
-        </Route>
-        <Route path="*">
-          <Header transparent={true} position="absolute" />
-          <NotFound message="Denne siden ekisterer ikke (ennå)" link="/" linkTitle="Gå til hovedsiden" />
-          <Footer />
-        </Route>
-      </Switch >
-    );
-  }
-
+function App() {
+  return (
+    <Switch>
+      <Route path="/prosjekter">
+        <ProjectRouter />
+      </Route>
+      <Route exact path="/cv">
+        <Header />
+        <CV />
+        <Footer />
+      </Route>
+      <Route exact path="/">
+        <Header />
+        <Home />
+        <Footer />
+      </Route>
+      <Route path="*">
+        <Header transparent={true} position="absolute" />
+        <NotFound message="Denne siden ekisterer ikke (ennå)" link="/" linkTitle="Gå til hovedsiden" />
+        <Footer />
+      </Route>
+    </Switch >
+  );
 }
 
-export default withRouter(App);
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { initializeApp } from 'firebase/app';
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAKyjXJR5Sh6gqlH8nLDyZ2d7qWNg2YV44",
-  authDomain: "homepage-a0343.firebaseapp.com",
-  projectId: "homepage-a0343",
-  storageBucket: "homepage-a0343.appspot.com",
-  messagingSenderId: "691600299529",
-  appId: "1:691600299529:web:3e336a0448cc636fae8ad8"
-};
-
-const app = initializeApp(firebaseConfig);
-console.log(app);
 
 ReactDOM.render(
   <React.StrictMode>

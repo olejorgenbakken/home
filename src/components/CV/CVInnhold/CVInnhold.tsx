c2f1eb3d1ce46db4ba79909d4f521b801fd6b095
OJ <olejorgenbakken@icloud.com>
2021-10-20 20:53:46 +0200

    kun et årstall om det er det samme

--- src/components/CV/CVInnhold/CVInnhold.tsx
+++ src/components/CV/CVInnhold/CVInnhold.tsx
@@ -9,14 +9,28 @@ interface Props {
 }
 
 const CVInnhold = ({ tittel, sted, start, slutt, beskrivelse }: Props) => {
-    return (
-        <article>
-            <header>
+    const sjekkTiden = () => {
+        if (start !== slutt) {
+            return (
                 <label>
                     <time>{start}</time>
                     &nbsp;–&nbsp;
                     <time>{slutt}</time>
                 </label>
+            )
+        } else {
+            return (
+                <label>
+                    <time>{slutt}</time>
+                </label>
+            )
+        }
+    }
+
+    return (
+        <article>
+            <header>
+                {sjekkTiden()}
                 <h4 className="role">{tittel}</h4>
                 <h5 className="place">{sted}</h5>
             </header>
